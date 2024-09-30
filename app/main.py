4c01ab8f6e096c250b123622125e6d3e1b3bb2dd
Jonce <josezanunez@gmail.com>
2024-09-30 15:16:57 -0500

    chore(app): integrar el endpoint de logs en la aplicación principal
    - Se añadió el router de logs a la aplicación FastAPI para exponer el endpoint de obtención de logs

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
-from app.api.routes import notifications, ui  # Importa los módulos de rutas notifications y ui
+from app.api.routes import notifications, ui, logs  # Importa los módulos de rutas notifications y ui
 
 app = FastAPI(title="VTEX External Marketplace Integration") # Definición de la instancia principal.
 
-# Incluir rutas (actualizaremos estas rutas más adelante cuando los archivos estén listos)
-app.include_router(notifications.router, prefix="/notifications", tags=["Notifications"])
+# Crear la base de datos al iniciar
+#create_database()
+
+# Incluir rutas
+app.include_router(notifications.router, prefix="/notifications", tags=["Notifications"]) # El prefix es el trozo de path que se agregaría a la ruta a la cual estamos redireccionando el request
 app.include_router(ui.router, tags=["UI"])
+app.include_router(logs.router, tags=["Logs"])  # Incluir el router de logs
 
 # Punto de entrada de la aplicación
 if __name__ == "__main__":
