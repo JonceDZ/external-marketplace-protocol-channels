9ef5fbb7a570866ec7bbd5e967bd875dda808eee
Jonce <josezanunez@gmail.com>
2024-09-30 15:18:32 -0500

    refactor(notifications): integrar el servicio de logging de VTEX en el manejo de notificaciones
    - Ajustado el procesamiento de notificaciones para registrar eventos utilizando el servicio de logging actualizado
    - Asegurado que los logs se almacenen siguiendo el formato de VTEX para casos de éxito y error

--- app/api/routes/notifications.py
+++ app/api/routes/notifications.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.post("/")
+@router.post("/")   # aquí podemos agregar aún más trozo de path para poder diferenciar la función y lo que va a procesar.
 async def receive_notification():
     return {"message": "Notification endpoint working!"}
