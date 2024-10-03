f016f150d240d4f3fb48e58b4bb941d8ee2ecbec
Jonce <josezanunez@gmail.com>
2024-10-03 13:02:50 -0500

    feat(database_models): se incluyó el campo de product_id en la estructura de la tabla, para que sea almacenado ese campo también

--- app/models/database_models.py
+++ app/models/database_models.py
@@ -6,6 +6,7 @@ class LogEntry(Base):
     __tablename__ = "logs"
 
     id = Column(Integer, primary_key=True, index=True)
+    OperationId = Column(Integer, nullable=False) ############
     Operation = Column(String, nullable=False)
     Direction = Column(String, nullable=False)
     ContentSource = Column(Text, nullable=True)
@@ -20,6 +21,7 @@ class Product(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     sku_id = Column(Integer, unique=True, index=True, nullable=False)
+    product_id = Column(Integer, nullable=False) ######################
     name = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     price = Column(Float, nullable=True)
