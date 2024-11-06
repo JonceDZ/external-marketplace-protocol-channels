e35c23c0db633270e8defe09b9572d53844cb0b4
Jonce <josezanunez@gmail.com>
2024-11-06 09:58:24 -0500

    Borrado de archivos que no se usaron.

--- app/test_settings.py
+++
@@ -1,6 +0,0 @@
-from app.config.settings import settings
-
-print("VTEX_APP_KEY:", settings.vtex_app_key)
-print("VTEX_APP_TOKEN:", settings.vtex_app_token)
-print("VTEX_ACCOUNT_NAME:", settings.vtex_account_name)
-print("VTEX_API_URL:", settings.vtex_api_url)
