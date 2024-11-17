6ef487e3999148f6c02807d0cce73c00bcc252f6
Jonce <josezanunez@gmail.com>
2024-11-17 18:44:45 -0500

    Actualización de la inicialización de la app para manejar credenciales opcionales
    - Se ajustó la validación de las credenciales de VTEX al iniciar la app para soportar entornos con variables de entorno vacías.
    - Se mejoró la validación para evitar errores críticos al arrancar el servidor.

--- app/main.py
+++ app/main.py
@@ -1,25 +1,21 @@
-from app.services.vtex_api import VTEXAPI
-from fastapi import FastAPI
-from app.api.routes import notifications, ui, logs, products, orders  # Importa los módulos de rutas notifications y ui
-from app.db.database import create_database 
+# app/main.py
 
-app = FastAPI(title="VTEX External Marketplace Integration") # Definición de la instancia principal.
+from fastapi import FastAPI
+from app.api.routes import notifications, ui, logs, products, orders
+from app.db.database import create_database
 
 # Crear la base de datos al iniciar
 create_database()
 
+app = FastAPI(title="VTEX External Marketplace Integration")
+
 # Incluir rutas
-app.include_router(notifications.router, prefix="/notifications", tags=["Notifications"]) # El prefix es el trozo de path que se agregaría a la ruta a la cual estamos redireccionando el request
+app.include_router(notifications.router, prefix="/notifications", tags=["Notifications"])
 app.include_router(ui.router, tags=["UI"])
 app.include_router(logs.router, tags=["Logs"])
 app.include_router(products.router, prefix="/products", tags=["Products"])
 app.include_router(orders.router, prefix="/orders", tags=["Orders"])
 
-# Punto de entrada de la aplicación
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
-# Validación de headers al iniciar la app
-vtex_api = VTEXAPI()
-vtex_api.validate_headers()
+# Montar archivos estáticos
+from fastapi.staticfiles import StaticFiles
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
