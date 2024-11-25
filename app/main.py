de0f777071823b8a5c9f2e5c9600d429c8af69ae
Jonce <josezanunez@gmail.com>
2024-11-25 18:11:24 -0500

    UI: Rediseño completo de la interfaz basado en las guías de estilo de VTEX. Cambios en header, footer, estilos de productos y configuración inicial. Adición del logo VTEX.

--- app/main.py
+++ app/main.py
@@ -3,11 +3,14 @@
 from fastapi import FastAPI
 from app.api.routes import notifications, ui, logs, products, orders
 from app.db.database import create_database
+from fastapi.staticfiles import StaticFiles
+
 
 # Crear la base de datos al iniciar
 create_database()
 
 app = FastAPI(title="VTEX External Marketplace Integration")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 # Incluir rutas
 app.include_router(notifications.router, prefix="/notifications", tags=["Notifications"])
@@ -15,7 +18,3 @@ app.include_router(ui.router, tags=["UI"])
 app.include_router(logs.router, tags=["Logs"])
 app.include_router(products.router, prefix="/products", tags=["Products"])
 app.include_router(orders.router, prefix="/orders", tags=["Orders"])
-
-# Montar archivos estáticos
-from fastapi.staticfiles import StaticFiles
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
